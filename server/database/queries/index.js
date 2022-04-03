33147b062460f4bbcd6cc1fa45fc0084fb3f0122
AbdallahGot <abdallah897107@gmail.com>
2022-04-03 16:00:56 +0300

    add getProduct and getAllProducts queries , add controllers for get product and get all products relates # 7

--- server/database/queries/index.js
+++ server/database/queries/index.js
@@ -1,5 +1,9 @@
 const deleteProductQuery = require('./deleteProductQuery');
 const editProduct = require('./editProductQuery');
 const addProductQuery = require('./addProductQuery');
+const getProduct = require('./getProduct');
+const getAllProducts = require('./getAllProducts');
 
-module.exports = { deleteProductQuery, editProduct, addProductQuery };
+module.exports = {
+  deleteProductQuery, editProduct, addProductQuery, getProduct, getAllProducts,
+};
--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,11 @@
 const Router = require('express').Router();
 
-const { editProduct, deleteProduct, addProduct } = require('./controllers');
+const {
+  editProduct, deleteProduct, addProduct, getProduct, getAllProducts,
+} = require('./controllers');
 
-Router.route('/product').post(addProduct);
+Router.route('/product').post(addProduct).get(getAllProducts);
 
-Router.route('/product/:id').patch(editProduct).delete(deleteProduct);
+Router.route('/product/:id').patch(editProduct).delete(deleteProduct).get(getProduct);
 
 module.exports = Router;
