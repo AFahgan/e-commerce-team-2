2078a0a300122a6272f9f121d4e7f3ed94177289
amjad <amjedyehia1998@gmail.com>
2022-04-04 17:20:21 +0300

    add products to build.sql

--- server/database/config/build.sql
+++ server/database/config/build.sql
@@ -7,7 +7,76 @@ CREATE TABLE products (
   image TEXT NOT NULL,
   category VARCHAR(50) NOT NULL,
   price DECIMAL NOT NULL
-);  
-INSERT INTO products (id, name, description, image, category, price) VALUES (1, 'men t-shirt ', 'Lorem Ipsum has been the industrys standard dummy text', 'https://hips.hearstapps.com/vader-prod.s3', 'men', 125.25), (2, 'women t-shirt ', 'took a galley of type and scrambled it to make a type', 'https://hips.hearstapps.com/vader-prod.s3', 'women', 125.25);
-
+);
+insert into products (name, description, image, category, price)
+values (
+    'green couch',
+    'Cool hipster green couch sofa with brown wooden legs on wooden floor',
+    'https://i.imgur.com/ZDUr9aN.jpg',
+    'sofa',
+    123.23
+  ),
+  (
+    'leather couch',
+    'Cool hipster green couch sofa with brown wooden legs on wooden floor',
+    'https://i.imgur.com/NtI1r65.png',
+    'sofa',
+    90.55
+  ),
+  (
+    'silver couch',
+    'Cool hipster green couch sofa with brown wooden legs on wooden floor',
+    'https://i.imgur.com/WQlr2sS.png',
+    'sofa',
+    80.33
+  ),
+  (
+    'brown couch',
+    'Cool hipster green couch sofa with brown wooden legs on wooden floor',
+    'https:/i.imgur.com/fpMeT8a.png',
+    'sofa',
+    30.23
+  ),
+  (
+    'white chair',
+    'Cool hipster green chair with brown white legs on wooden floor',
+    'https:/i.imgur.com/fpMeT8a.png',
+    'chair',
+    30.23
+  ),
+  (
+    'purple chair',
+    'Cool hipster brown chair with brown white legs on wooden floor',
+    'https://i.imgur.com/xJiV5U1.png',
+    'chair',
+    20.13
+  ),
+  (
+    'fancy chair',
+    'Cool hipster brown chair with brown white legs on wooden floor',
+    'https://i.imgur.com/kSPqT8c.png',
+    'chair',
+    20.13
+  ),
+  (
+    'brown chair',
+    'Cool hipster brown chair with brown white legs on wooden floor',
+    'https://i.imgur.com/RoHIo7B.png',
+    'chair',
+    70.13
+  ),
+  (
+    'black chair',
+    'Cool hipster brown chair with brown white legs on wooden floor',
+    'https://i.imgur.com/ElcPA7s.png',
+    'chair',
+    20.13
+  ),
+  (
+    'small chair',
+    'Cool hipster brown chair with brown white legs on wooden floor',
+    'https://i.imgur.com/O9qL2L9.png',
+    'chair',
+    10.13
+  );
 COMMIT;
