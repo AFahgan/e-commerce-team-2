6e7fdc49a893daf6a2fe32aa007804da9ccb52e8
amjad <amjedyehia1998@gmail.com>
2022-04-07 10:37:54 +0300

    fix

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -7,24 +7,26 @@ const Landing = ({ checkState, handleOnClick }) => {
       <div className='welcome-container'>
         <div className='left-container'>
           <h1 className='main-text'>Hello! What would you like to buy?</h1>
-          <p className='website-description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+          <p className='website-description'>
+            The idea of changing in general is exhausting itself. So in our website we try to
+            make it a comfortable step . we all know how annoying and hard it's to refurnish
+            your home or maybe you're someone who's about to get married and your wife keeps
+            complaining and winning about getting so this is idea of our website , in our
+            website there are many options for furnisher ,so It got easier.
+          </p>
 
           <a href='#products'>
-          < Button text='Start Shopping' />
+            <Button text='Start Shopping' />
           </a>
 
           <span className='between-tow-component'>&emsp;</span>
-          < Button  text='Log In Seller' handleOnClick={handleOnClick}/>
-          {checkState? (
-          < LogInForm handleOnClick={handleOnClick}/>
-          ): (
-            null
-            )}
+          <Button text='Log In Seller' handleOnClick={handleOnClick} />
+          {checkState ? <LogInForm handleOnClick={handleOnClick} /> : null}
         </div>
         <div className='right-container'>
-          <img src="../assets/landing-img.png" alt="main img in the website " />
+          <img src='../assets/landing-img.png' alt='main img in the website ' />
         </div>
       </div>
-    )
+    );
 }
 export default Landing;
